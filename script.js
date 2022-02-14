61206a4001e6921571ff359626e5709b64516c18
prajjwalsinghtangar <prajjwalsinghtangar@gmail.com>
2022-02-14 13:33:00 -0500

    adding loader

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const quoteContainer = document.getElementById('quote-container')
 const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
-const twitterBtn = document.getElementById('twitte')
+const twitterBtn = document.getElementById('twitter')
 const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader');
+
+// Show Loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 let apiQuotes = [];
 
 // new quote
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     // Check Author if unknown
     if (!quote.author){
@@ -21,12 +35,14 @@ function newQuote() {
     }else{
         authorText.classList.remove('long-quote');
     }
-
+    // hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 //quotes function
 async function getQuotes() {
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try{
         const response = await fetch(apiUrl);
@@ -36,5 +52,15 @@ async function getQuotes() {
       // catch error  
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listner
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
 
 getQuotes();
